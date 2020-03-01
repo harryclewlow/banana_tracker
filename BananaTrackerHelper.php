9079af9dbe89057dfac8aabc8a21a3e5bfb98473
Harry Clewlow <harryclewlow@Harrys-MacBook-Pro.local>
2020-03-01 12:34:13 +0000

    BT-1 : Create BannaTrackerHelper

---
+++ BananaTrackerHelper.php
@@ -0,0 +1,60 @@
+<?php
+
+class BananaTrackerHelper
+{
+    /**
+     * @param string $routeJson
+     * @return array
+     * @throws Exception
+     */
+    public function getSortedRoute(string $routeJson): array
+    {
+        $route = $this->convertRouteFromJson($routeJson);
+        $sortedList = [];
+        $fromList = array_column($route, 'from');
+        $toList = array_column($route, 'to');
+        $index = $this->getFirstStopInRouteIndex($fromList, $toList);
+        $sortedList[] = $fromList[$index];
+
+        while (count($sortedList) < count($route) + 1) {
+            $currentStop = $toList[$index];
+            $sortedList[] = $currentStop;
+            $index = array_search($currentStop, $fromList);
+        }
+
+        return $sortedList;
+    }
+
+    /**
+     * @param string $route
+     * @return array
+     * @throws Exception
+     */
+    private function convertRouteFromJson(string $route): array
+    {
+        $decodedRoute = json_decode($route, true);
+
+        if ($decodedRoute === null || json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('json is invalid');
+        }
+
+        return $decodedRoute;
+    }
+
+    /**
+     * @param array $fromList
+     * @param array $toList
+     * @return int
+     * @throws Exception
+     */
+    private function getFirstStopInRouteIndex(array $fromList, array $toList): int
+    {
+        $uniqueStops = array_diff($fromList, $toList);
+
+        if (count($uniqueStops) !== 1) {
+            throw new Exception('Invalid route');
+        }
+
+        return array_keys($uniqueStops)[0];
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# banana_tracker
+# Banana Tracker
+
+## Example JSON
+[{
+	"from": "Adolfo Suárez Madrid–Barajas Airport, Spain",
+	"to": "London Heathrow, UK"
+}, {
+	"from": "Fazenda São Francisco Citros, Brazil",
+	"to": "São Paulo–Guarulhos International Airport, Brazil"
+}, {
+	"from": "London Heathrow, UK",
+	"to": "Loft Digital, London, UK"
+}, {
+	"from": "Porto International Airport, Portugal",
+	"to": "Adolfo Suárez Madrid–Barajas Airport, Spain"
+}, {
+	"from": "São Paulo–Guarulhos International Airport, Brazil",
+	"to": "Porto International Airport, Portugal"
+}]
